d73c67400c52561ccf3d9e0e7d5b66a576f5e574
Thanmayi Kondreddy <146209969+thanmayi01@users.noreply.github.com>
2024-10-01 11:36:14 +0530

    Update HelloWorld.php

--- PHP/HelloWorld.php
+++ PHP/HelloWorld.php
@@ -3,7 +3,17 @@
 <body>
 
 <?php
+// different print functions in php
+// 0.echo keyword
 echo "Hello World!";
+// 1.echo function
+echo ("Hello World");
+// this function also prints type of variable and length if it's there
+print_r("Hello World!");
+// print function
+print("Hello World!");
+// var_dump function prints variable type and length
+var_dump("Hello World!");
 ?>
 
 </body>
