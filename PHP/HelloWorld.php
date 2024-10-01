78e410c213be8fed1447aa88ab87ec22ad2e97eb
Thanmayi Kondreddy <146209969+thanmayi01@users.noreply.github.com>
2024-10-01 11:42:02 +0530

    Update HelloWorld.php
    Different print function in php

--- PHP/HelloWorld.php
+++ PHP/HelloWorld.php
@@ -3,7 +3,17 @@
 <body>
 
 <?php
+// different ways to print helloworld in php
+//1. echo keyword
 echo "Hello World!";
+//2.echo funrction
+echo("Hello World!");
+//3.print function
+print("Hello World!");
+//4. print_r variable typr and length if its possible
+print_r("Hello World!");
+//5.var_dump function return variable type and length if its possible
+var_dump("Hello World!");
 ?>
 
 </body>
